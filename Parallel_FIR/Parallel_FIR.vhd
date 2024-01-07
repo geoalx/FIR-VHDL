f2b9681e7d3555c6b155708e1dd33e15c04571ee
Stefania Skorda <94705222+stefaniaskorda@users.noreply.github.com>
2024-01-07 19:01:14 +0200

    Update Parallel_FIR.vhd

--- Parallel_FIR/Parallel_FIR.vhd
+++ Parallel_FIR/Parallel_FIR.vhd
@@ -2,7 +2,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
+-- NTUA
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
